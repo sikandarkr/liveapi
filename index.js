5d5382024591033d7bb9dca25efc01a9c4112ada
unknown <royalsikandar25@gmail.com>
2021-01-18 00:34:06 +0530

    first commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 let port = process.env.PORT || 3000;
 
 app.get("/demo", (req, res) => {
-    return res.json({ "data": "records...." });
+    return res.json({ data: [{ "name": "sikandar" }, { "name": "Ranjeet" }, { "name": "Amit" }, {}] });
 });
 
 app.listen(port, () => {
