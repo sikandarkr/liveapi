b9b59e7e70c2d8f1794033aa120d2fc8c9b8ad41
unknown <royalsikandar25@gmail.com>
2021-01-20 09:04:34 +0530

    changes made

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,6 +1,6 @@
 module.exports = {
     getList: (req, res, next) => {
-        return res.json({ name: "Sikandar", surname: "Kumar", address: "East Champaran motihari" })
+        return res.json({ name: "Sikandar", surname: "Kumar", address: "East Champaran motihari" });
     }
 }
 
