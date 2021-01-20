26c2f564eecd6f016fddac233bd03608c0479b64
unknown <royalsikandar25@gmail.com>
2021-01-20 08:48:33 +0530

    changes text message

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,5 +1,5 @@
 module.exports = {
     getList: (req, res, next) => {
-        return res.json({ "data": "nothing for now" })
+        return res.json({ "data": "Hi sikandar ,How are you doing?" })
     }
 }
